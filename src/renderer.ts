ff20a63a67a2432aecbb1f16d7fb384b511a4e7b
gitenxx <25375525+gitenxx@users.noreply.github.com>
2024-01-06 16:04:23 +0300

    Sprites changing and better markdown handling

--- src/renderer.ts
+++ src/renderer.ts
@@ -18,10 +18,19 @@ window.onload = function () {
 
 function handleText() {
   const text: IteratorResult<string, void> = lines.next();
+  markdownSeeker(text)
+}
+
+function markdownSeeker(text: IteratorResult<string, void>) {
   switch (text.value, true) {
     case (text.value[0] == "#"):
       if (!text.done) (document.getElementById('charName') as HTMLInputElement).innerHTML = text.value.replace(/#+/, '');
       (document.getElementById('clickableText') as HTMLInputElement).value = lines.next().value as string;
+      break;
+    case (text.value[0] == "~"):
+      var sprt = ('./content/sprites/' + text.value.replace(/~+/, '')) 
+      if (!text.done) (document.getElementById('sprite') as HTMLImageElement).src = sprt;
+      markdownSeeker(lines.next());
       break;
     default:
       if (!text.done) (document.getElementById('clickableText') as HTMLInputElement).value = text.value as string;
